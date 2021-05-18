89d3425686d735230bf118fde777960f40faafa0
Antheius-AT <53980774+Antheius-AT@users.noreply.github.com>
2021-05-18 16:35:47 +0200

    Refactored code to make test case for negative numbers working

--- StringCalculator/Calculator.cs
+++ StringCalculator/Calculator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace StringCalculator
@@ -19,11 +20,22 @@ namespace StringCalculator
             var splitNumbers = numbers.Replace("//", string.Empty).Split(splitChars, StringSplitOptions.RemoveEmptyEntries);
 
             var result = 0;
+            var negatives = new List<int>();
 
             for (int i = 0; i < splitNumbers.Length; i++)
             {
+                var current = int.Parse(splitNumbers[i]);
+                if (current < 0)
+                {
+                    negatives.Add(current);
+                    continue;
+                }
+
                 result += int.Parse(splitNumbers[i]);
             }
+
+            if (negatives.Count > 0)
+                throw new ArgumentException($"negatives not allowed: {negatives}");
 
             return result;
         }
