57a74ab270d766f3618a2127194b12616574b8e9
Antheius-AT <53980774+Antheius-AT@users.noreply.github.com>
2021-05-18 15:36:40 +0200

    Added first test, but test failed

--- CalculatorTests/UnitTest1.cs
+++ CalculatorTests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using StringCalculator;
 
 namespace CalculatorTests
 {
@@ -10,9 +11,12 @@ namespace CalculatorTests
         }
 
         [Test]
-        public void Test1()
+        [TestCase("1,2")]
+        [TestCase("5,6")]
+        [TestCase("10,20")]
+        public void Does_Numbers_Separated_With_Comma_Return_Add(string input)
         {
-            Assert.Pass();
+            Calculator.Add(input);
         }
     }
 }
---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringCalculator
+{
+    public static class Calculator
+    {
+        public static int Add (string numbers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
